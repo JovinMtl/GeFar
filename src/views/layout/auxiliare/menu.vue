aeedfac579be44b3cd8393d2f31815640f4e7df2
JovinMtl <nsanzumukizath@gmail.com>
2024-04-22 16:11:27 +0200

    defining menu buttons

---
+++ src/views/layout/auxiliare/menu.vue
@@ -0,0 +1,92 @@
+<template>
+    <div class="menuContainer">
+        <!-- //here is the menu -->
+        <a @click.prevent="" href="http://" target="_blank" rel="noopener noreferrer">
+            <div :class="button1 ? 'clicked':'unclicked'" @click="turnA">
+            Vendre  
+        </div>
+        </a>
+        <a @click.prevent="" href="http://" target="_blank" rel="noopener noreferrer"
+        style="color: white;">
+            <div :class="button2 ? 'clicked':'unclicked'" @click="turnB">Approvisionner</div>
+        </a>
+        <a @click.prevent="" href="http://" target="_blank" rel="noopener noreferrer">
+           <div :class="button3 ? 'clicked':'unclicked'" @click="turnC">
+                Vérifier 
+            </div> 
+        </a>
+            
+        
+    </div>
+</template>
+<script>
+import { ref } from 'vue'
+export default {
+    setup() {
+        const button1 = ref(true)
+        const button2 = ref(false)
+        const button3 = ref(false)
+
+        const turnA = ()=>{
+            button1.value = true
+            button2.value = false
+            button3.value = false
+        }
+        const turnB = ()=>{
+            button2.value = true
+            button1.value = false
+            button3.value = false
+        }
+        const turnC = ()=>{
+            button3.value = true
+            button2.value = false
+            button1.value = false
+        }
+
+        return {
+            button1, button2, button3,
+            turnA, turnB, turnC,
+        }
+    },
+}
+</script>
+<style scoped>
+.menuContainer{
+    height: 30px;
+    width: 40vw;
+    /* background-color: yellow; */
+    text-align: center;
+}
+.unclicked{
+    display: inline-block;
+    width: 24%;
+    height: 100%;
+    background-color: black;
+    margin: 0px 4px;
+    border-top-left-radius: 15px;
+    border-top-right-radius: 15px;
+    align-content: center;
+    align-items: center;
+    text-align: center;
+    /* margin: auto; */
+    /* padding: 0px; */
+}
+.clicked{
+    display: inline-block;
+    width: 24%;
+    height: 100%;
+    margin: 0px 4px;
+    border-top-left-radius: 15px;
+    border-top-right-radius: 15px;
+    align-content: center;
+    align-items: center;
+    text-align: center;
+    background-color: rgba(255, 255, 255, 0.474);
+}
+a > .clicked {
+    color: rgba(0, 0, 255, 0.516);
+}
+a > .unclicked{
+    color: white;
+}
+</style>
