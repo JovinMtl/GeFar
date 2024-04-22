7de6fddf64c40e17f45f03fbf1496a28ba8c0c3a
JovinMtl <nsanzumukizath@gmail.com>
2024-04-22 12:27:49 +0200

    redirecting to feuille

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,20 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import HomePage from '../views/HomePage.vue'
+import page from '../views/layout/page.vue'
 
 const routes = [
   {
     path: '/',
-    redirect: '/home'
+    redirect: '/feuille'
   },
   {
     path: '/home',
     name: 'Home',
     component: HomePage
+  },
+  {
+    path : '/feuille',
+    component: page
   }
 ]
 
