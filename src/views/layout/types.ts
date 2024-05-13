dc07ba362c1ec84c0a1cd0de5d4507d9a5c134c9
JovinMtl <nsanzumukizath@gmail.com>
2024-05-13 11:29:01 +0200

    starting UmutiSet interface definition

---
+++ src/views/layout/types.ts
@@ -0,0 +1,10 @@
+//This is for types and interfaces definitions
+
+
+
+export interface UmutiSet {
+    id : number;
+    code_umuti : string;
+    name_umuti : string;
+    date_last_vente : string
+}
