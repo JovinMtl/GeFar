311c93e0553b7c9561e22dd59c0431676dbe91d9
JovinMtl <nsanzumukizath@gmail.com>
2024-05-13 11:43:27 +0200

    UmutiSet interface defined

--- src/views/layout/types.ts
+++ src/views/layout/types.ts
@@ -1,10 +1,22 @@
 //This is for types and interfaces definitions
 
-
+// type dateUmuti = new Date
 
 export interface UmutiSet {
     id : number;
     code_umuti : string;
     name_umuti : string;
-    date_last_vente : string
+    description_umuti : string;
+    date_last_vente : Object;
+    price_in : number;
+    price_out : number;
+    difference : number;
+    qte_entrant_big : number;
+    quantite_restant : number;
+    ratio_type : number;
+    type_in : string;
+    type_out : string;
+    type_umuti : string;
+    location : string;
+    lot : object[];
 }
