63992871a500befa1573bba3fcc43bee9b1dfd89
JovinMtl <nsanzumukizath@gmail.com>
2024-05-16 20:44:55 +0200

    clearing the console.logs whenever a watch triggers an effect

--- src/views/layout/page.vue
+++ src/views/layout/page.vue
@@ -282,7 +282,6 @@ export default {
         watch(panier_client.value, (value)=>{
             // Update the Total on panier whenever we add or remove any umuti on Panier
             total_panier_client.value = update_total_client()
-            console.log("The Panier_client has new change: ", total_panier_client.value)
         })
 
         return {
