ce920f95504106ee553742a9709adc76cdaac7a8
JovinMtl <nsanzumukizath@gmail.com>
2024-05-17 14:39:41 +0200

    removing IonSelect in approv component

--- src/views/operations/approv.vue
+++ src/views/operations/approv.vue
@@ -2,26 +2,8 @@
     <div>
         Ici on gere des approvisionnement
         <br> <br>
-        <input class="inp" type="text" placeholder="Nom du medicament">
-        <br><br>
-        <input class="inp" type="text" placeholder="Cp, Gel, Ovule, Sirop, Inj ?">
-        <ion-select placeholder="Type de medicament" mode="ios">
-                        <ion-select-option>
-                            Cp
-                        </ion-select-option>
-                        <ion-select-option>
-                            Gel
-                        </ion-select-option>
-                        <ion-select-option>
-                            Ovule
-                        </ion-select-option>
-                        <ion-select-option>
-                            Sirop
-                        </ion-select-option>
-                        <ion-select-option>
-                            Inj
-                        </ion-select-option>
-                    </ion-select>
+        <input type="text" placeholder="Nom du medicament">
+        
         <br> <br>
         <label>Type de medicament</label>
         <select placeholder="jojdkfjk" value="Ovuledfdfdfdf">
@@ -33,50 +15,43 @@
             <!-- <option>Inj</option> -->
         </select>
         <br> <br>
-        <input class="inp" type="color" placeholder="Nom du medicament">
+        <textarea style="border-radius: 15px;" name="description" placeholder="Description du medicament" id="" cols="30" rows="10"></textarea>
+        <input type="text" placeholder="ratio: 1 : 1">
         <br><br>
-        <input class="inp" type="text" placeholder="Nom du medicament">
+        <input type="text" placeholder="Nom du medicament">
         <br> <br>
-        <textarea name="description" placeholder="Description du medicament" id="" cols="30" rows="10"></textarea>
-        <input class="inp" type="text" placeholder="ratio: 1 : 1">
-        <br><br>
-        <input class="inp" type="text" placeholder="Nom du medicament">
+        <input type="text" placeholder="Type in">
         <br> <br>
-        <input class="inp" type="text" placeholder="Type in">
-        <br> <br>
-        <input class="inp" type="text" placeholder="Type out">
+        <input type="text" placeholder="Type out">
         <br><br>
         <br> <br>
-        <input class="inp" type="text" placeholder="Price in">
+        <input type="text" placeholder="Price in">
         <br> <br>
-        <input class="inp" type="text" placeholder="Price out">
+        <input type="text" placeholder="Price out">
         <br> <br>
-        <input class="inp" type="text" placeholder="Quantite Initial">
+        <input type="text" placeholder="Quantite Initial">
         <br> <br>
-        <input class="inp" type="text" placeholder="Localisation">
+        <input type="text" placeholder="Localisation">
         <br>
         <label>Date d'exp. </label>
-        <input class="inp" type="date" placeholder="Nom du medicament">
+        <input type="date" placeholder="Nom du medicament">
         <br>
     </div>
 </template>
 <script>
-import { IonSelect, IonSelectOption,} from '@ionic/vue'
-import { close } from 'ionicons/icons'
 export default {
-    components: {
-        IonIcon, IonSelectOption, IonSelect, 
-    },
     setup() {
         return {
-            close,
+
         }
     },
 }
 </script>
 
 <style scoped>
-.inp{
-    color: white;
+input, textarea{
+    background-color: rgba(25, 255, 25, 0.6);
+    background-color: rgba(25, 255, 25, 0.521);
+    color: black;
 }
 </style>
