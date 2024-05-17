af2be3c5762ebd35ae49add88294e05c9e941a7e
JovinMtl <nsanzumukizath@gmail.com>
2024-05-17 11:56:20 +0200

    coloring the input text color

--- src/views/operations/approv.vue
+++ src/views/operations/approv.vue
@@ -1,6 +1,16 @@
 <template>
     <div>
         Ici on gere des approvisionnement
-        <input type="text" placeholder="Nom du medicament">
+        <br> <br>
+        <input class="inp" type="text" placeholder="Nom du medicament">
+        <br><br>
+        <input class="inp" @click="console.log('You clicked an input')" type="text" placeholder="Nom du medicament">
+        <br>
     </div>
 </template>
+
+<style scoped>
+.inp{
+    color: white;
+}
+</style>
