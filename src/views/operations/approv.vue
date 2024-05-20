5183fc7f58475c9c676b5e785b33a566e26cfc68
JovinMtl <nsanzumukizath@gmail.com>
2024-05-20 12:11:27 +0200

    in case we have a selected umuti result, then hide the existing form

--- src/views/operations/approv.vue
+++ src/views/operations/approv.vue
@@ -1,49 +1,52 @@
 <template>
     <div>
         <h6>Ici on gere des approvisionnement</h6>
+        <section v-if="!selected_search">
+            <br> <br>
+            <input v-model="umutiName" type="text" placeholder="Nom du medicament">
+            <hr>
+            <ul style="text-align: right;" v-if="!selected_search">
+                <button class="btnResearch" v-for="(umuti, index) in imiti_result" 
+                    :id="'a' + index" @click="selectSearch">
+                    {{ umuti.name_umuti }} {{ umuti.code_umuti }} {{ umuti.description_umuti }}
+                </button>
+            </ul>
         
-        <br> <br>
-        <input v-model="umutiName" type="text" placeholder="Nom du medicament">
-        <hr>
-        <ul style="text-align: right;" v-if="!selected_search">
-            <button class="btnResearch" v-for="(umuti, index) in imiti_result" 
-                :id="'a' + index" @click="selectSearch">
-                {{ umuti.name_umuti }} {{ umuti.code_umuti }} {{ umuti.description_umuti }}
-            </button>
-        </ul>
-        
-        <br> <br>
-        <label>Type de medicament</label>
-        <span style="margin-right: .1rem;">&nbsp;</span>
-        <select style="background-color: white" placeholder="Type" value="Ovule">
-            <option>Cp</option>
-            <option>Gel</option>
-            <option>Ovule</option>
-            <option>Sirop</option>
-            <option>Inj</option>
-            <!-- <option>Inj</option> -->
-        </select>
-        <br> <br>
-        <textarea style="border-radius: 15px;" name="description" placeholder="Description du medicament" id="" cols="20" rows="3"></textarea>
-        <input type="text" placeholder="ratio: 1 : 1">
-        <br><br>
-        <input type="text" placeholder="Type in: Carton">
-        <br> <br>
-        <input type="text" placeholder="Type out : Plaquette">
-        <br><br>
-        <br> <br>
-        <input type="text" placeholder="Price in: (Type_in)">
-        <br> <br>
-        <input type="text" placeholder="Price out : (Type_in)">
-        <br> <br>
-        <input type="text" placeholder="Quantite Initial">
-        <br> <br>
-        <input type="text" style="margin-bottom: 5px;" placeholder="Localisation">
-        <br>
-        <label>Date d'exp. </label> 
-        <!-- <br> -->
-        <input type="date" placeholder="Nom du medicament">
-        <br><hr>
+            <br> <br>
+            <label>Type de medicament</label>
+            <span style="margin-right: .1rem;">&nbsp;</span>
+            <select style="background-color: white" placeholder="Type" value="Ovule">
+                <option>Cp</option>
+                <option>Gel</option>
+                <option>Ovule</option>
+                <option>Sirop</option>
+                <option>Inj</option>
+                <!-- <option>Inj</option> -->
+            </select>
+            <br> <br>
+            <textarea style="border-radius: 15px;" name="description" placeholder="Description du medicament" id="" cols="20" rows="3"></textarea>
+            <input type="text" placeholder="ratio: 1 : 1">
+            <br><br>
+            <input type="text" placeholder="Type in: Carton">
+            <br> <br>
+            <input type="text" placeholder="Type out : Plaquette">
+            <br><br>
+            <br> <br>
+            <input type="text" placeholder="Price in: (Type_in)">
+            <br> <br>
+            <input type="text" placeholder="Price out : (Type_in)">
+            <br> <br>
+            <input type="text" placeholder="Quantite Initial">
+            <br> <br>
+            <input type="text" style="margin-bottom: 5px;" placeholder="Localisation">
+            <br>
+            <label>Date d'exp. </label> 
+            <!-- <br> -->
+            <input type="date" placeholder="Nom du medicament">
+            <br><hr>
+        </section>
+        <section v-if="selected_search">
+        </section>
     </div>
 </template>
 <script>
