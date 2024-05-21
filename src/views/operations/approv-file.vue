c587f5e9896e6fa7e5c7c3fd9c2e59d6853ebc50
JovinMtl <nsanzumukizath@gmail.com>
2024-05-21 11:01:47 +0200

    making approv-file component to have IonIcon and close icon

--- src/views/operations/approv-file.vue
+++ src/views/operations/approv-file.vue
@@ -1,6 +1,24 @@
 
 <template>
     <div>
+        <span class="closeBtn" style="">
+                        <ion-icon :src="close" @click="closeApprov"></ion-icon>
+                    </span>
         Here we input the file
     </div>
 </template>
+
+<script>
+import { IonIcon } from '@ionic/vue'
+import { close } from 'ionicons/icons'
+export default {
+    components: {
+        IonIcon,
+    },
+    setup() {
+        return {
+            close,
+        }
+    },
+}
+</script>
