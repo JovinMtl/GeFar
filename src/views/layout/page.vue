5172e428ca5aa044c93103155aa1491320277828
JovinMtl <nsanzumukizath@gmail.com>
2024-05-21 12:09:02 +0200

    catching the event emitted and handle it with closeApproFile function

--- src/views/layout/page.vue
+++ src/views/layout/page.vue
@@ -3,7 +3,7 @@
         <ion-content>
             <div class="mainApprob" v-if="approvStatus">
                 <div class="approFile" style="">
-                    <appro-file></appro-file>
+                    <appro-file @approFileClose="closeApproFile"></appro-file>
                 </div>
                 <div class="approClass">
                     <div class="headerApprov" style="display: block;position: sticky; top: 0px; width: 100%; height: 10%; background-color: white; align-items: center; align-content: center; text-align: center;">
