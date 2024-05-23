53387ae1ce7d7b0e1edfedc9978913f8af0693ee
JovinMtl <nsanzumukizath@gmail.com>
2024-05-23 12:19:26 +0200

    removing the style of notif in this component to the global file

--- src/views/operations/approv-file.vue
+++ src/views/operations/approv-file.vue
@@ -77,7 +77,7 @@
             </div>
         </div>
         
-        <div class="notif" style="position: absolute; width: 500px;height: 10rem; background-color: white;left:8rem">
+        <div class="notif" style="">
             <p></p>
         </div>
         
