7099880434d8fc3b1a73766e42047907ee225190
JovinMtl <nsanzumukizath@gmail.com>
2024-05-23 09:52:49 +0200

    checking that the file selected is of type text/csv, if not then process nothing but inform.

--- src/views/operations/approv-file.vue
+++ src/views/operations/approv-file.vue
@@ -101,47 +101,52 @@ export default {
         const fileHandler = async ()=>{
             const selectedFile = document.getElementById('file1').files[0]
             console.log("The filename is :", selectedFile.type)
-            const file_blob = URL.createObjectURL(selectedFile)
-            // Create a new FileReader object for reading the selected file
-            let fileReader = new FileReader();
+            if(selectedFile.type === 'text/csv'){
+                const file_blob = URL.createObjectURL(selectedFile)
+                // Create a new FileReader object for reading the selected file
+                let fileReader = new FileReader();
 
-            // Read the content of the selected file
-            fileReader.readAsText(selectedFile);
+                // Read the content of the selected file
+                fileReader.readAsText(selectedFile);
 
-            // Define a function to be executed when the file is read
-            fileReader.onload = () => {
-                // console.log("The content:", fileReader.result);
-                let content = fileReader.result
-                let lines = content.split('\n')
-                let first_line = lines[1]
-                let second_line = lines[2]
-                let obj_array = []
-                let line_length = second_line.split(',').length
-                let jove = 'Nom'
-                // let obj = {
-                //     [jove ? 'Nomme':'Nome']: 'Nsanzumukiza'
-                // }
-                // console.log("The obj is :", obj)
-                console.log("the first column is :", second_line.split(',')[1])
-                console.log("The length of the line is : ", line_length)
-                
-                lines.forEach(element => {
-                    let columns = element.split(',')
-                    let obj = {}
-                    for(let i=1; i < line_length; i++){
-                        if(columns[i]){
-                            obj[first_line.split(',')[i]] = columns[i]
+                // Define a function to be executed when the file is read
+                fileReader.onload = () => {
+                    // console.log("The content:", fileReader.result);
+                    let content = fileReader.result
+                    let lines = content.split('\n')
+                    let first_line = lines[1]
+                    let second_line = lines[2]
+                    let obj_array = []
+                    let line_length = second_line.split(',').length
+                    let jove = 'Nom'
+                    // let obj = {
+                    //     [jove ? 'Nomme':'Nome']: 'Nsanzumukiza'
+                    // }
+                    // console.log("The obj is :", obj)
+                    console.log("the first column is :", second_line.split(',')[1])
+                    console.log("The length of the line is : ", line_length)
+                    
+                    lines.forEach(element => {
+                        let columns = element.split(',')
+                        let obj = {}
+                        for(let i=1; i < line_length; i++){
+                            if(columns[i]){
+                                obj[first_line.split(',')[i]] = columns[i]
+                            }
                         }
-                    }
-                    
-                    if(obj[first_line.split(',')[1]]){
-                        obj_array.push(obj)
-                    }
-                    
-                });
-                imiti_loaded.value = obj_array
-                console.log("The obj array is : ", obj_array)
-            };
+                        
+                        if(obj[first_line.split(',')[1]]){
+                            obj_array.push(obj)
+                        }
+                        
+                    });
+                    imiti_loaded.value = obj_array
+                    console.log("The obj array is : ", obj_array)
+                };
+            } else {
+                console.log("You should CHOOSE a text/csv file")
+            }
+            
             
             
         }
