03f5aaec5aec46cf01d5328b625a7738a3275ccf
JovinMtl <nsanzumukizath@gmail.com>
2024-05-28 16:07:23 +0200

    logOne is ready in its place

--- src/views/operations/controle.vue
+++ src/views/operations/controle.vue
@@ -10,7 +10,12 @@
         </div>
         <div class="controBody" style="background-color: yellowgreen; height: 92%; width: 100%;">
             <div class="controlIcons" style="display: inline-block; width: 20%; height: 100%; border:2px solid white ">
-                <div class="logOne" style="display: block;background-color: aqua; width: 100%;height: 20%;"></div>
+                <div class="logOne" style="display: block;background-color: transparent; 
+                width: 100%;height: 20%; padding: 5px 5px; border-bottom: 2px solid white">
+                    <ion-icon :src="statsChartOutline" style="position: absolute; font-size: 50pt;margin-left: 3vw;"></ion-icon>
+                    <span style="position: absolute;font-size: 15pt ;left: 9vw; margin-top: 5vh;">Les ventes</span>
+                </div>
+                
             </div>
             <div class="controlIcons" style="display: inline-flex; width: 80%; height: 100%; border:2px solid white "></div>
         </div>
