7b0b75719ca61cd245fbc9881f6a810d3d0a31de
JovinMtl <nsanzumukizath@gmail.com>
2024-05-29 11:00:40 +0200

    restructuring the hook to return only the function of kuvoma

--- src/views/hooks/kuvoma.js
+++ src/views/hooks/kuvoma.js
@@ -1,65 +1,7 @@
 
-import { UmutiSet } from '../layout/types.ts'
 
-export default function useKuvoma(simple=true){
-    let imiti_for_search = []
-    kuvomaImiti ()
-
-    if (simple){
-        return [
-            imiti_for_search, imitiset,
-        ]
-    }
-}
-const updateImitiSet = ()=>{
-    imitiset.value = []
-    // console.log("ImitiSet has lenght: ", data.value.length)
-    let  i = 1
-    data.value.forEach(element => {
-        let obj:UmutiSet = {
-            'code_umuti' : element.code_umuti,
-            'id' : i,
-            'name_umuti' : element.name_umuti,
-            'description_umuti' : element.description_umuti,
-            'date_last_vente' : new Date(element.date_last_vente),
-            'price_in' : element.price_in,
-            'price_out' : element.price_out,
-            'difference' : element.difference,
-            'qte_entrant_big' : element.qte_entrant_big,
-            'quantite_restant' : element.quantite_restant,
-            'ratio_type' : element.ratio_type,
-            'type_in' : element.type_in,
-            'type_out' : element.type_out,
-            'type_umuti' : element.type_umuti,
-            'location' : element.location,
-            'lot' : element.lot
-        }
-        imitiset.value.push(obj)
-        // console.log("voici: ", obj)
-    });
-    console.log("Compiled : ", imitiset.value)
-}
-
-
-const update_imiti_for_search = ()=>{
-    imitiset.value.forEach(element => {
-        let obj = {
-            'code_umuti' : element.code_umuti,
-            'date_last_vente' : element.date_last_vente,
-            'description_umuti' : element.description_umuti,
-            'location': element.location,
-            'lot': element.lot,
-            'name_umuti': (element.name_umuti),
-            'price_in' : element.price_in,
-            'price_out' : element.price_out,
-            'qte_entrant_big' : element.qte_entrant_big,
-            'quantite_restant': element.quantite_restant,
-            'type_in': element.type_in,
-            'type_out' : element.type_out
-        }
-        
-        imiti_for_search.push(obj)
-    });
+export default function useKuvoma(){
+    return kuvomaImiti
 }
 
 const kuvomaImiti = async () => {
@@ -70,14 +12,11 @@ const kuvomaImiti = async () => {
         const response = await fetch(`${base}/${prefix}`)
         
         if (response.ok){
-            data.value = await response.json()
-            console.log("THings are well received", data.value)
-            updateImitiSet()
-            update_imiti_for_search()
+            let data = await response.json()
+            return data
             // console.log("ImitiSet has lenght: ", data.value.length)
         }
     } catch (value){
         console.log("somehting may not be well because :", value)
     }
 }
-kuvomaImiti ()
