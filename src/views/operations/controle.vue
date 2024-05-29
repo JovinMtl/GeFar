3cb5ae3c5eb9f88cce9623729e4bc40454d821c4
JovinMtl <nsanzumukizath@gmail.com>
2024-05-29 11:22:21 +0200

    removing the definition of kuvoma in controle.vue

--- src/views/operations/controle.vue
+++ src/views/operations/controle.vue
@@ -46,20 +46,5 @@ import { close, statsChartOutline, snowOutline, thermometer, pricetagOutline} fr
 import useKuvoma from '../hooks/kuvoma'
 
 
-// const ukuvoma = async () => {
-//     const base = '//127.0.0.1:8002'
-//     const prefix = 'api/out/dispo/'
 
-//     try {
-//         const response = await fetch(`${base}/${prefix}`)
-        
-//         if (response.ok){
-//             let data = await response.json()
-//             return data
-//             // console.log("ImitiSet has lenght: ", data.value.length)
-//         }
-//     } catch (value){
-//         console.log("somehting may not be well because :", value)
-//     }
-// }
 </script>
