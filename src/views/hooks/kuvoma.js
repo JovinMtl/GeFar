6c34c9212731b8be78d1d0137d1b6b6d4df25b5b
JovinMtl <nsanzumukizath@gmail.com>
2024-05-29 21:41:06 +0200

    returning an array

--- src/views/hooks/kuvoma.js
+++ src/views/hooks/kuvoma.js
@@ -1,27 +1,28 @@
 
-import { ref } from 'vue'
-const data = ref(null)
+import { ref, watch } from 'vue'
 
-export default function useKuvoma(){
-    kuvomaImiti()
-    console.log("useKuvoma got: ", data.value)
-    return [
-        data.value,
-    ]
-}
 
-const kuvomaImiti = async () => {
-    const base = '//127.0.0.1:8002'
-    const prefix = 'api/out/dispo/'
+export default async function useKuvoma(){
+    const data = ref(null)
 
-    try {
-        const response = await fetch(`${base}/${prefix}`)
-        
-        if (response.ok){
-            data.value = await response.json()
-            // console.log("ImitiSet has lenght: ", data.value)
+    const kuvomaImiti = async () => {
+        const base = '//127.0.0.1:8002'
+        const prefix = 'api/out/dispo/'
+
+        try {
+            const response = await fetch(`${base}/${prefix}`)
+            
+            if (response.ok){
+                data.value = await response.json()
+                // console.log("ImitiSet has lenght: ", data.value)
+            }
+        } catch (value){
+            console.log("somehting may not be well because :", value)
         }
-    } catch (value){
-        console.log("somehting may not be well because :", value)
     }
+
+    console.log("useKuvoma got: ", data.value)
+    return [data, kuvomaImiti]
+    
 }
+
