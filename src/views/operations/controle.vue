f3cf972640b5d04772ad82568dd35de66f75b9d4
JovinMtl <nsanzumukizath@gmail.com>
2024-05-29 11:06:41 +0200

    declaring kuvoma and use it on click

--- src/views/operations/controle.vue
+++ src/views/operations/controle.vue
@@ -10,7 +10,7 @@
         </div>
         <div class="controBody" style="background-color: yellowgreen; height: 92%; width: 100%;">
             <div class="controlIcons" style="display: inline-block; width: 20%; height: 100%; border:2px solid white ">
-                <div class="logoOne" @click="useKuvoma"
+                <div class="logoOne" @click="kuvoma"
                     style="display: block;background-color: transparent; 
                     width: 100%;height: 20%; padding: 5px 5px; border-bottom: 2px solid white">
                     <ion-icon :src="statsChartOutline" style="position: absolute; font-size: 50pt;margin-left: 3vw;"></ion-icon>
@@ -44,4 +44,7 @@
 import { IonIcon } from '@ionic/vue'
 import { close, statsChartOutline, snowOutline, thermometer, pricetagOutline} from 'ionicons/icons'
 import useKuvoma from '../hooks/kuvoma'
+
+
+const kuvoma = useKuvoma()
 </script>
