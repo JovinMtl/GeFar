1cf035f916c91865dd611fba429b3cc901d3b119
JovinMtl <nsanzumukizath@gmail.com>
2024-05-29 10:42:41 +0200

    reverting list-imiti to the initial working state

--- src/views/operations/list-imiti.vue
+++ src/views/operations/list-imiti.vue
@@ -31,7 +31,7 @@ export default defineComponent ({
         const data = reactive({})
         const imitiset:UmutiSet[] = ref([])
         let codes = new Array()
-        // let imiti_for_search = []
+        let imiti_for_search = []
 
         const needUpdate = inject('needUpdate_list')
         const need_search = inject('needSearch')
@@ -644,56 +644,60 @@ export default defineComponent ({
         }
         kuvomaImiti ()
 
-        // const updateImitiSet = ()=>{
-        //     imitiset.value = []
-        //     // console.log("ImitiSet has lenght: ", data.value.length)
-        //     let  i = 1
-        //     data.value.forEach(element => {
-        //         let obj:UmutiSet = {
-        //             'code_umuti' : element.code_umuti,
-        //             'id' : i,
-        //             'name_umuti' : element.name_umuti,
-        //             'description_umuti' : element.description_umuti,
-        //             'date_last_vente' : new Date(element.date_last_vente),
-        //             'price_in' : element.price_in,
-        //             'price_out' : element.price_out,
-        //             'difference' : element.difference,
-        //             'qte_entrant_big' : element.qte_entrant_big,
-        //             'quantite_restant' : element.quantite_restant,
-        //             'ratio_type' : element.ratio_type,
-        //             'type_in' : element.type_in,
-        //             'type_out' : element.type_out,
-        //             'type_umuti' : element.type_umuti,
-        //             'location' : element.location,
-        //             'lot' : element.lot
-        //         }
-        //         imitiset.value.push(obj)
-        //         // console.log("voici: ", obj)
-        //     });
-        //     console.log("Compiled : ", imitiset.value)
-        // }
+        const updateImitiSet = ()=>{
+            imitiset.value = []
+            // console.log("ImitiSet has lenght: ", data.value.length)
+            let  i = 1
+            data.value.forEach(element => {
+                let obj:UmutiSet = {
+                    'code_umuti' : element.code_umuti,
+                    'id' : i,
+                    'name_umuti' : element.name_umuti,
+                    'description_umuti' : element.description_umuti,
+                    'date_last_vente' : new Date(element.date_last_vente),
+                    'price_in' : element.price_in,
+                    'price_out' : element.price_out,
+                    'difference' : element.difference,
+                    'qte_entrant_big' : element.qte_entrant_big,
+                    'quantite_restant' : element.quantite_restant,
+                    'ratio_type' : element.ratio_type,
+                    'type_in' : element.type_in,
+                    'type_out' : element.type_out,
+                    'type_umuti' : element.type_umuti,
+                    'location' : element.location,
+                    'lot' : element.lot
+                }
+                imitiset.value.push(obj)
+                // console.log("voici: ", obj)
+            });
+            console.log("Compiled : ", imitiset.value)
+        }
 
-        // const update_imiti_for_search = ()=>{
-        //     imitiset.value.forEach(element => {
-        //         let obj = {
-        //             'code_umuti' : element.code_umuti,
-        //             'date_last_vente' : element.date_last_vente,
-        //             'description_umuti' : element.description_umuti,
-        //             'location': element.location,
-        //             'lot': element.lot,
-        //             'name_umuti': (element.name_umuti),
-        //             'price_in' : element.price_in,
-        //             'price_out' : element.price_out,
-        //             'qte_entrant_big' : element.qte_entrant_big,
-        //             'quantite_restant': element.quantite_restant,
-        //             'type_in': element.type_in,
-        //             'type_out' : element.type_out
-        //         }
+        const update_imiti_for_search = ()=>{
+            imitiset.value.forEach(element => {
+                let obj = {
+                    'code_umuti' : element.code_umuti,
+                    'date_last_vente' : element.date_last_vente,
+                    'description_umuti' : element.description_umuti,
+                    'location': element.location,
+                    'lot': element.lot,
+                    'name_umuti': (element.name_umuti),
+                    'price_in' : element.price_in,
+                    'price_out' : element.price_out,
+                    'qte_entrant_big' : element.qte_entrant_big,
+                    'quantite_restant': element.quantite_restant,
+                    'type_in': element.type_in,
+                    'type_out' : element.type_out
+                }
                 
-        //         imiti_for_search.push(obj)
-        //     });
-        // }
+                imiti_for_search.push(obj)
+            });
+        }
+        // updateImitiSet()
 
+        // onUpdated(()=>{
+        //     // updateImitiSet()
+        // })
         watch(shouldUpdate, (value)=>{
             console.log("onUpdated, needToUpdate:..", value)
             if(value){
