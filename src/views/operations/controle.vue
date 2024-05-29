0203105708b9ff2cc12922bf635575c8ed155649
JovinMtl <nsanzumukizath@gmail.com>
2024-05-29 09:48:20 +0200

    logoFour established

--- src/views/operations/controle.vue
+++ src/views/operations/controle.vue
@@ -23,8 +23,14 @@
                 </div>
 
                 <div class="logoThree" style="display: block;background-color: transparent; 
-                width: 100%;height: 20%; padding: 5px 5px; border-bottom: 2px solid white">
+                    width: 100%;height: 20%; padding: 5px 5px; border-bottom: 2px solid white">
                     <ion-icon :src="thermometer" style="position: absolute; font-size: 50pt;margin-left: 3vw;"></ion-icon>
+                    <span style="position: absolute;font-size: 15pt ;left: 9vw; margin-top: 5vh;">Les ventes</span>
+                </div>
+
+                <div class="logoFour" style="display: block;background-color: transparent; 
+                    width: 100%;height: 20%; padding: 5px 5px; border-bottom: 2px solid white">
+                    <ion-icon :src="pricetagOutline" style="position: absolute; font-size: 50pt;margin-left: 3vw;"></ion-icon>
                     <span style="position: absolute;font-size: 15pt ;left: 9vw; margin-top: 5vh;">Les ventes</span>
                 </div>
             </div>
