4f9b76e7d1631eaa323d9f012869930f5a0803f5
JovinMtl <nsanzumukizath@gmail.com>
2024-05-29 10:17:45 +0200

    bringing update_imiti_for_search function

--- src/views/hooks/kuvoma.js
+++ src/views/hooks/kuvoma.js
@@ -1,5 +1,24 @@
 
-
+const update_imiti_for_search = ()=>{
+    imitiset.value.forEach(element => {
+        let obj = {
+            'code_umuti' : element.code_umuti,
+            'date_last_vente' : element.date_last_vente,
+            'description_umuti' : element.description_umuti,
+            'location': element.location,
+            'lot': element.lot,
+            'name_umuti': (element.name_umuti),
+            'price_in' : element.price_in,
+            'price_out' : element.price_out,
+            'qte_entrant_big' : element.qte_entrant_big,
+            'quantite_restant': element.quantite_restant,
+            'type_in': element.type_in,
+            'type_out' : element.type_out
+        }
+        
+        imiti_for_search.push(obj)
+    });
+}
 
 const kuvomaImiti = async () => {
     const base = '//127.0.0.1:8002'
