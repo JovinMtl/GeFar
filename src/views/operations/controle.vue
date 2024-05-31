164e091df05fd95a17b7820135d32713101daaf4
JovinMtl <nsanzumukizath@gmail.com>
2024-05-31 10:48:07 +0200

    reducing the size of the date fields

--- src/views/operations/controle.vue
+++ src/views/operations/controle.vue
@@ -25,11 +25,11 @@
                 </div>
             <div class="recherche" style="display: inline-flex; margin: 0px 20px;">
                 debut
-                <input type="date"  style="margin-top: -2px;">
+                <input type="date"  style="margin-top: -2px; width: 80px;">
             </div>
             <div class="recherche" style="display: inline-flex; margin: 0px 10px;">
                 fin
-                <input type="date" style="margin-top: -2px;">
+                <input type="date" style="margin-top: -2px; width: 80px;">
             </div>
             <div class="recherche" style="display: inline-flex; margin: 0px 10px;">
                 
