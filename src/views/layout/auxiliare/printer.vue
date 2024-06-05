6a8d0a28cf7b75fd51470711a0ef6164d7215c3d
JovinMtl <nsanzumukizath@gmail.com>
2024-06-05 11:33:41 +0200

    sample template code implemented

---
+++ src/views/layout/auxiliare/printer.vue
@@ -0,0 +1,24 @@
+<template>
+    <main>
+      <section class="chapter">
+        <h2>About</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipiscing elit. Duis nibh tortor</p>
+      </section>
+  
+      <section class="chapter">
+        <h2>Chapter 1</h2>
+        <p>Lorem ipsum dolor sit amet</p>
+      </section>
+  
+      <section class="chapter">
+        <h2>Chapter 2</h2>
+        <p>consectetur adipiscing elit</p>
+      </section>
+  
+      <section class="chapter">
+        <h2>Chapter 3</h2>
+        <p>Duis nibh tortor, pellentesque eu suscipit vel</p>
+      </section>
+    </main>
+  </template>
+  
