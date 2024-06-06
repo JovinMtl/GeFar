31c19e84987c3108d45001251799ff1455a37646
JovinMtl <nsanzumukizath@gmail.com>
2024-06-06 06:52:07 +0200

    new version of code implemented

---
+++ src/views/layout/auxiliare/printer.vue
@@ -0,0 +1,49 @@
+<template>
+  <main>
+    <section class="chapter">
+      <h2>About</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipiscing elit. Duis nibh tortor</p>
+    </section>
+
+    <section class="chapter">
+      <h2>Chapter 1</h2>
+      <p>Lorem ipsum dolor sit amet</p>
+    </section>
+
+    <section class="chapter">
+      <h2>Chapter 2</h2>
+      <p>consectetur adipiscing elit</p>
+    </section>
+
+    <section class="chapter">
+      <h2>Chapter 3</h2>
+      <p>Duis nibh tortor, pellentesque eu suscipit vel</p>
+    </section>
+  </main>
+</template>
+
+<script setup>
+import { onMounted } from 'vue';
+import { Previewer } from 'pagedjs';
+
+onMounted(() => {
+  let paged = new Previewer();
+
+  paged.preview().then((flow) => {
+    console.log('Rendered', flow.total, 'pages.');
+  });
+});
+</script>
+  
+<style scoped>
+@page {
+  size: A6;
+  /* margin: 20mm 15mm 26mm 15mm; */
+  margin: 20mm 15mm 26mm 15mm;
+}
+</style>
+
+h2 {
+    break-before: page;
+  }
+  
---
+++ src/views/layout/auxiliare/printero.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+      <button @click="printContent">Print</button>
+      <div ref="printArea" id="pri">
+        <!-- This is the content you want to print -->
+        <h1>Hello, World!</h1>
+        <p>This is a paragraph to be printed.</p>
+      </div>
+    </div>
+  </template>
+
+<script setup>
+import { ref } from 'vue';
+
+const printContent = () => {
+  const printableElement = document.getElementById('pri');
+
+  if (printableElement) {
+    printableElement.style.display = 'block';
+    window.print();
+    printableElement.style.display = 'none';
+  } else {
+    alert('Could not find printable content.');
+  }
+};
+</script>
+  
+<style scoped>
+@page {
+  size: A6;
+  /* margin: 20mm 15mm 26mm 15mm; */
+  margin: 20mm 15mm 26mm 15mm;
+}
+</style>
+
+h2 {
+    break-before: page;
+  }
+  
