fae3dc4b0b7ad8ea1e6cf6bfcc7d641993b1bbe0
JovinMtl <nsanzumukizath@gmail.com>
2024-06-12 13:30:47 +0200

    converted facturier styles into sass.

--- src/views/operations/facturier.vue
+++ src/views/operations/facturier.vue
@@ -9,7 +9,7 @@
                 <!-- Voici: {{ message }}. <br>
                 Defini: {{ commandePatient }} -->
                 <div class="content">
-
+                    <div class="number"></div>
                 </div>
             </div>
         </div>
@@ -24,50 +24,41 @@ console.log("Facturier INITIALIZED: ", props.value)
 
 // Has to launch print functionality at the mount cycle
 </script>
-<style scoped>
-/* .factuContent > h3 {
-    display: none;
-} */
-.factuTitle{
-    display: flex;
-    align-content: center;
-    background: linear-gradient(to right, black, green, green);
-    background-clip: text;
-    -webkit-text-fill-color: transparent;
-    width: 100%;
-    height: 50px;
-}
+<style lang="scss" scoped>
 
 @media only print {
     @page{
         size: 8cm 10cm
     }
-    .factuContent > h3 {
-        display: flex;
-    }
     .factureContainer{
-        /* display: flex; */
         width: 100%;
         height: 100%;
         background-color: gray;
-        /* justify-content: center; */
         font-size: .4rem;
-        /* margin: 20px; */
+
+        .factuHead{
+            text-align: center;
+            margin: 25px 0px;
+            font-size: 1.1rem;
+            background-color: red;
+        }
+        .factuBody{
+            padding: 15px;
+
+            .content{
+                background: white;
+                width: 100%;
+                height: 20px
+
+                .number{
+                }
+            }
+        }
     }
-    .factuHead{
-        text-align: center;
-        margin: 25px 0px;
-        font-size: 1.1rem;
-        background-color: red;
-    }
-    .factuBody{
-        padding: 15px
-    }
-    .content{
-        background: white;
-        width: 100%;
-        height: 20px
-    }
+    
+    
+    
+    
 }
 
 </style>
