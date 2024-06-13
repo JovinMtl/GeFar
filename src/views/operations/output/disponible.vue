b03ff028cef312539d9fcdb122fe444ebea339d4
JovinMtl <nsanzumukizath@gmail.com>
2024-06-13 14:26:26 +0200

    made the three main container

--- src/views/operations/output/disponible.vue
+++ src/views/operations/output/disponible.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="dispoContainer">
         Here we print disponible data
+        <div class="dispoHeader"></div>
+        <div class="dispoBody"></div>
+        <div class="dispoFooter"></div>
     </div>
 </template>
 <script setup lang="ts">
