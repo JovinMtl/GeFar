56ce2e889adcfe0640d90b4591c67bf6ae22fedb
JovinMtl <nsanzumukizath@gmail.com>
2024-06-13 22:04:29 +0200

    the authorizer is well displayed

--- src/views/operations/facturier.vue
+++ src/views/operations/facturier.vue
@@ -218,7 +218,7 @@ const closeFacturier = ()=>{
             display: block;
             width: 100%;
             height: 10%;
-            background-color: #0bf532;
+            padding-left: 15px;
         }
         .factuFooter{
             width: 100%;
