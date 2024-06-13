477fd79c8564ceca95700303314c9570bb61b629
JovinMtl <nsanzumukizath@gmail.com>
2024-06-13 15:02:24 +0200

    using a 'mono' font-family.

--- src/views/operations/output/disponible.vue
+++ src/views/operations/output/disponible.vue
@@ -1,12 +1,15 @@
 <template>
     <div class="dispoContainer">
-        <!-- Here we print disponible data -->
-        <div class="dispoHeader"></div>
+        <div class="dispoHeader">
+           <span style="font-weight: 600">DAWA - PHARMA : RAPPORT</span>  <br>
+           <span style="color: gray;"><u>Disponible du 01 au 15 Juin.</u> </span> 
+        </div>
         <div class="dispoBody"></div>
         <div class="dispoFooter"></div>
     </div>
 </template>
 <script setup lang="ts">
+// Date_debut & date_fin should be given through props
 </script>
 <style scoped lang="scss">
 
@@ -23,8 +26,12 @@
 
         .dispoHeader{
             width: 100%;
-            height: 5%;
+            height: 8%;
             background-color: white;
+            text-align: center;
+            align-content: center;
+            font-size: 1.375rem;
+            font-family: monospace;
         }
     }
 }
