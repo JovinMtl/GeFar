cc8ec3011ba168b6111fec464e8cf5f4a3cf36a1
JovinMtl <nsanzumukizath@gmail.com>
2024-06-13 14:23:00 +0200

    disponible component init .

---
+++ src/views/operations/output/disponible.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="dispoContainer">
+        Here we print disponible data
+    </div>
+</template>
+<script setup lang="ts">
+</script>
+<style scoped lang="scss">
+
+</style>
