1e2c5bdac39245d2a9052f01ce4233021bd8bf2d
JovinMtl <nsanzumukizath@gmail.com>
2024-06-14 20:03:56 +0200

    centering a container for authentification

--- src/views/auth/login.vue
+++ src/views/auth/login.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="LogContainer">
         ICi authentification
+        <div class="authentif"></div>
     </div>
 </template>
 <script setup lang="ts"></script>
@@ -12,10 +13,23 @@
 }
 @media screen and (min-width: 800px) {
     .LogContainer{
+        display: flex;
         width: 100%;
         height: 100%;
         background-color: black;
+        background-color: rgba(0, 255, 0, 0.692);
         position: relative;
+        justify-content: center;
+        align-items: center;
+
+        .authentif{
+            width: 30%;
+            height: 90%;
+            background-color: white;
+            position: absolute;
+            border-radius: 15px;
+            box-shadow: 0 0 45px black;
+        }
     }
 }
 </style>
