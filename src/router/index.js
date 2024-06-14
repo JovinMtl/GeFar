ba247378b838f9f9a1ecc03ac5e7438609e80380
JovinMtl <nsanzumukizath@gmail.com>
2024-06-14 19:45:22 +0200

    login component routed

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from '@ionic/vue-router';
 import page from '../views/layout/page.vue'
 // import print from '../views/layout/auxiliare/printer.vue'
 import disponible from '../views/operations/output/disponible.vue'
+import Logi from '../views/auth/login.vue'
+import { components } from 'vuetify/dist/vuetify-labs.js';
 
 const routes = [
   {
     path: '/',
-    redirect: '/print'
+    redirect: '/logi'
   },
   {
     path : '/home',
@@ -15,6 +17,10 @@ const routes = [
   {
     path : '/print',
     component: disponible
+  },
+  {
+    path : '/logi',
+    component: Logi,
   }
 ]
 
