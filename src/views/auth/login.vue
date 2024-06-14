2f334cf69cc04f156fb9e638d8a804e04a8690cd
JovinMtl <nsanzumukizath@gmail.com>
2024-06-14 19:52:05 +0200

    added media query for min-width

--- src/views/auth/login.vue
+++ src/views/auth/login.vue
@@ -3,3 +3,16 @@
         ICi authentification
     </div>
 </template>
+<script setup lang="ts"></script>
+<style scoped lang="scss">
+@media screen and (max-width: 400px) {
+    .LogContainer{
+        background-color: white;
+    }
+}
+@media screen and (min-width: 800px) {
+    .LogContainer{
+        background-color: black;
+    }
+}
+</style>
