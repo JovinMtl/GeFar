60f96c3fe49b74c6577cf8f475577f08f13586c8
JovinMtl <nsanzumukizath@gmail.com>
2024-06-15 13:37:58 +0200

    made se connecter button to react on hover effect.

--- src/views/auth/login.vue
+++ src/views/auth/login.vue
@@ -4,7 +4,9 @@
         <div class="authentif">
             <div class="username">
                 <label for="input">Username</label> <br>
-                <input type="text" style="padding: 15px 30px; border-radius: 15px;border-style: hidden;">
+                <input type="text" style="padding: 15px 30px; 
+                    border-radius: 15px;border-style: hidden;
+                    background-color: rgb(28, 35, 49);">
             </div>
             <div class="username">
                 <label for="input">Password</label> <br>
@@ -13,8 +15,12 @@
                     border-style: hidden; background-color: rgb(28, 35, 49);">
             </div>
             <div class="confirmationContainer">
-                <div class="one" style="border-right: 1px solid black;">s'inscrire</div>
-                <div class="one">se connecter</div>
+                <!-- <div class="one" style="border-right: 1px solid black;">s'inscrire</div>
+                <div class="one">se connecter</div> -->
+                <!-- se connecter -->
+            </div>
+            <div class="enter">
+                <div class="btn">se connecter</div>
             </div>
         </div>
     </div>
@@ -32,6 +38,7 @@
         width: 100%;
         height: 100%;
         background-color: rgba(0, 255, 0, 0.692);
+        // background-color: rgba(128, 128, 128, 0.397);
         position: relative;
         justify-content: center;
         align-items: center;
@@ -55,11 +62,53 @@
                 height: 40px;
                 background: white;
                 color: black;
+                justify-content: center;
+                align-items: center;
 
+                &:hover{
+                    font-weight: 800;
+                }
                 .one{
                     width: 50%;
                     height: 100%;
                     align-content: center;
+
+                    &:hover{
+                        // background: rgba(0, 255, 0, 0.692);
+                        font-weight: 800;
+                    }
+                }
+            }
+            .enter{
+                display: flex;
+                width: 100%;
+                height: 40px;
+                margin-top: 30px;
+                justify-content: center;
+
+                .btn{
+                    display: flex;
+                    width: 50%;
+                    height: 100%;
+                    border-radius: 15px;
+                    justify-content: center;
+                    align-items: center;
+                    // border: 1px solid white;
+                    box-shadow: 0 0 5px white;
+                    align-content: center;
+                    background: linear-gradient(to right,black,rgb(99, 97, 97), white, gray);
+                    background-clip: text;
+                    -webkit-text-fill-color: transparent;
+
+                    &:active{
+                        background: rgba(255, 255, 255, 0.13);
+                        
+                    }
+                    &:hover{
+                        font-weight: 700;
+                        transform: rotateX(15deg);
+                    }
+
                 }
             }
         }
