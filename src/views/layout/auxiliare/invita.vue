80539680cd245e2bbf86c0fbed961886595690b6
JovinMtl <nsanzumukizath@gmail.com>
2024-06-20 08:18:48 +0200

    template code implemented.

---
+++ src/views/layout/auxiliare/invita.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="container">
+        <div class="containerInv">
+            <div class="invTitle">
+                Invitation à la soutenance de Mémoire de Master
+            </div>
+            <div class="contenu">Vous êtes cordialement invité à assister à la soutenance publique
+                de mon mémoire, realisé en vue de l' obtention du diplôme de Master en Sciences des Aliments et Nutrition,
+                option Securité alimentaire et Changement climatique.
+            </div>
+            <div class="imagesContainer">
+                <div class="logoUb">
+                    <!-- <img src="../../Documents/tmp/ub.png" alt=""> -->
+                </div>
+                <div class="photoOlivier"></div>
+                <div class="logoEansi"></div>
+            </div>
+            <div class="author">
+                <span style="font-weight: 800;
+                background: linear-gradient(to right, yellowgreen, black) ;
+                background-clip: text;
+                -webkit-text-fill-color: transparent;">
+                    Olivier NKURIKIYE
+                </span>
+                <span style="margin-left: -5px;">,</span>
+                <br>
+
+                <span style="font-size: 0.8rem; display: block;
+                    position: relative;top: -6px; font-style: italic;">
+                    présentera:
+                </span>
+                
+            </div>
+            <div class="theme">
+                <!-- <span style="font-size: 30px;">"</span>  -->
+                "ANALYSE DE LA RESILIENCE DES SYSTEMES SOCIO-ECOLOGIQUES DES
+                MENAGES RURAUX DU NORD DU BURUNDI."
+            </div>
+            <div class="lieu">
+                <div class="place">
+                    Campus Mutanga, dans l'une des salles 
+                    de l'EANSI/ EX FACAGRO.
+                </div>
+                <div class="dateLieu">
+                    09h00 <br> 
+                    28 Juin, 2024
+                </div>
+            </div>
+            <div class="enclosing">
+                Pour feter cette étape, retrouvons-nous autour 
+                d'une bière après la soutenance. 
+                <span style="font-weight: 500;">Lieu: Bar EX DCA</span>.
+            </div>
+            
+        </div>
+    </div>
+</template>
