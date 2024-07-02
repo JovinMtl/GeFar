bcce99b2d38e56fc5a26f5974c863d36a8fc09d0
JovinMtl <nsanzumukizath@gmail.com>
2024-07-02 15:35:37 +0200

    adding vue store

---
+++ src/store/index.js
@@ -0,0 +1,13 @@
+
+export default createStore({
+    state: {
+     url:"http://192.168.1.1:8000",
+     user : null,
+      },
+    mutations: {
+    },
+    actions: {
+    },
+    modules: {
+    }
+  })
