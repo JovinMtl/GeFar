e1910ed529773c1a90a56f7c555b58c4f899a716
JovinMtl <nsanzumukizath@gmail.com>
2024-07-03 10:46:16 +0200

    creating a User store

---
+++ src/store/user.js
@@ -0,0 +1,24 @@
+
+import { ref } from 'vue'
+import { defineStore } from 'pinia'
+
+
+export const useUserStore = defineStore('user', ()=>{
+    const username = ref('')
+    const accessToken = ref('')
+    const refreshToken = ref('')
+
+    const saveUsername = (value)=>{
+        username.value = value
+    }
+    const saveAccessToken = (value)=>{
+        accessToken.value = value
+    }
+    const saveRefreshToken = (value)=>{
+        refreshToken.value = value
+    }
+    return {
+        username, accessToken, refreshToken,
+        saveUsername, saveAccessToken, saveRefreshToken
+    }
+})
