321b8425754921c1a08509140118eeb9a29d9334
JovinMtl <nsanzumukizath@gmail.com>
2024-07-03 18:10:38 +0200

    removing invita component in the router.

--- src/router/index.js
+++ src/router/index.js
@@ -2,14 +2,12 @@ import { createRouter, createWebHistory } from '@ionic/vue-router';
 import page from '../views/layout/page.vue'
 // import print from '../views/layout/auxiliare/printer.vue'
 import disponible from '../views/operations/output/disponible.vue'
-import Logi from '../views/auth/login.vue'
-import Invita from '../views/layout/auxiliare/invita.vue'
-import { components } from 'vuetify/dist/vuetify-labs.js';
+import Login from '../views/auth/login.vue'
 
 const routes = [
   {
     path: '/',
-    redirect: '/home'
+    redirect: '/login'
   },
   {
     path : '/home',
@@ -20,12 +18,8 @@ const routes = [
     component: disponible
   },
   {
-    path : '/logi',
-    component: Logi,
-  },
-  {
-    path : '/invite',
-    component: Invita
+    path : '/login',
+    component: Login,
   }
 ]
 
