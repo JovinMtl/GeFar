34ede17860ac778debd29e1eb8ea782c62752129
JovinMtl <nsanzumukizath@gmail.com>
2024-07-18 12:27:06 +0200

    renaming remoteURL into baseURL

--- src/store/host.js
+++ src/store/host.js
@@ -1,4 +1,4 @@
 
 // export const localURL = "//127.0.0.1:8002"
-export const remoteURL = "//muteule.pythonanywhere.com"
+export const baseURL = "//muteule.pythonanywhere.com"
 // export const baseURL = "//127.0.0.1:8002"
