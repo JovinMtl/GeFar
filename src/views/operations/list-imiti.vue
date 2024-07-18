85a68cf75629809abfd6db397ba01adcaf756165
JovinMtl <nsanzumukizath@gmail.com>
2024-07-18 12:08:13 +0200

    pointed to data field of the returned response.

--- src/views/operations/list-imiti.vue
+++ src/views/operations/list-imiti.vue
@@ -637,8 +637,6 @@ export default defineComponent ({
         ]
 
         const kuvomaImiti = async () => {
-            // const base = '//muteule.pythonanywhere.com'
-            const base = '//127.0.0.1:8002'
             const prefix = 'api/out/dispo/'
 
             try {
@@ -650,10 +648,10 @@ export default defineComponent ({
                 
                 if (response.ok){
                     data.value = await response.json()
-                    console.log("THings are well received", data.value)
+                    data.value = data.value.data
+                    console.log("THings are well received", data.value.data)
                     updateImitiSet()
                     update_imiti_for_search()
-                    // console.log("ImitiSet has lenght: ", data.value.length)
                 }
             } catch (value){
                 console.log("somehting may not be well because :", value)
