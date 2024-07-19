88a6c27da2e7051ce7052d3a72058fb1a5154ec4
JovinMtl <nsanzumukizath@gmail.com>
2024-07-19 14:16:45 +0200

    calling the local server.

--- src/store/host.js
+++ src/store/host.js
@@ -1,4 +1,4 @@
 
 // export const localURL = "//127.0.0.1:8002"
-export const baseURL = "//muteule.pythonanywhere.com"
-// export const baseURL = "//127.0.0.1:8002"
+// export const baseURL = "//muteule.pythonanywhere.com"
+export const baseURL = "//127.0.0.1:8002"
