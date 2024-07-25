4624c83b864aa543c558a2aaee835d5d196c5e9a
JovinMtl <nsanzumukizath@gmail.com>
2024-07-25 05:52:18 +0200

    watching last_umutiEntree for further actiion.

--- src/store/host.js
+++ src/store/host.js
@@ -1,5 +1,5 @@
 
 // export const localURL = "//127.0.0.1:8002"
 // export const baseURL = "//muteule.pythonanywhere.com"
-// export const baseURL = "//127.0.0.1:8002"
-export const baseURL = "https://muteule.pythonanywhere.com"
+export const baseURL = "//127.0.0.1:8002"
+// export const baseURL = "https://muteule.pythonanywhere.com"
