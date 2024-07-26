f08ad05b0b61da0d7ab5b4d3fc7b4d9869abe71b
JovinMtl <nsanzumukizath@gmail.com>
2024-07-26 12:40:39 +0200

    made each circle to be bigger when being clicked.

--- src/views/operations/circle.vue
+++ src/views/operations/circle.vue
@@ -15,18 +15,19 @@
 .mainContainerCircle{
     display: grid;
     grid-template-columns: repeat(4, 1fr);
-    background: #333;
+    /* background: #333; */
     grid-column-gap: 20px;
     grid-row-gap: 20px;
     grid-auto-rows: 20px;
-    justify-content: center;
-    align-content: center;
+    justify-items: center;
 }
 .item1{
-    background-color: aqua;
     color: white;
     width: 30px;
     height: 30px;
     border-radius: 15px;
+}
+.item1:active{
+    scale: 1.3;
 }
 </style>
