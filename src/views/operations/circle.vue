7e24ea271d7f1a0587641e454fd1e4732378e4f8
JovinMtl <nsanzumukizath@gmail.com>
2024-07-26 12:42:59 +0200

    made the black circle

--- src/views/operations/circle.vue
+++ src/views/operations/circle.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="mainContainerCircle">
         <!-- Here are circle -->
-         <div class="item1"></div>
-         <div class="item1"></div>
-         <div class="item1"></div>
-         <div class="item1"></div>
+         <div class="item black"></div>
+         <div class="item yellow"></div>
+         <div class="item violet"></div>
+         <div class="item red"></div>
     </div>
 </template>
 
@@ -21,13 +21,16 @@
     grid-auto-rows: 20px;
     justify-items: center;
 }
-.item1{
+.item{
     color: white;
     width: 30px;
     height: 30px;
     border-radius: 15px;
 }
-.item1:active{
+.item:active{
     scale: 1.3;
+}
+.black{
+    background-color: black;
 }
 </style>
