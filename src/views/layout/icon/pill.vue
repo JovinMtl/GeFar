1538005efc3d90f58804f3f6696d31b3ccfb698a
JovinMtl <nsanzumukizath@gmail.com>
2024-08-01 10:46:27 +0200

    added pill icon

---
+++ src/views/layout/icon/pill.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { defineComponent } from 'vue';
+
+defineComponent({
+  name: 'CircumPill',
+});
+</script>
+<template>
+	<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
+		<path fill="currentColor" d="M19.31 4.691a5.5 5.5 0 0 0-7.78 0l-6.84 6.84a5.5 5.5 0 0 0 3.89 9.39a5.52 5.52 0 0 0 3.89-1.61l6.84-6.84a5.5 5.5 0 0 0 0-7.78m-.71 7.07l-3.42 3.42l-6.36-6.36L12.24 5.4a4.5 4.5 0 0 1 7.68 3.17a4.43 4.43 0 0 1-1.32 3.191" />
+	</svg>
+</template>
