41ce0229f9367813f8f7ad1a8e7368f04893a7ea
JovinMtl <nsanzumukizath@gmail.com>
2024-12-03 16:01:29 +0200

    pointing server to the localhost

--- src/store/host.js
+++ src/store/host.js
@@ -1,3 +1,3 @@
 
-// export const baseURL = "//127.0.0.1:8002"
-export const baseURL = "https://muteule.pythonanywhere.com"
+export const baseURL = "//127.0.0.1:8002"
+// export const baseURL = "https://muteule.pythonanywhere.com"
