1e28013878b027cdac26a0180bc690dd789335d0
JovinMtl <nsanzumukizath@gmail.com>
2024-12-06 15:29:52 +0200

    factorized the styles

--- src/views/dashBoard.vue
+++ src/views/dashBoard.vue
@@ -1,19 +1,46 @@
 <template>
     <div>
-        <div style="position:absolute;left: 80%;top:4px;
-            background-color: white; justify-items: center;
-            height: 25px; padding: 3px 2rem;color: black;
-            border-top-left-radius: 5px; border-top-right-radius: 5px;">
+        <div class="bg-w c-b dB-1">
             Dashboard</div>
-        <div style="display: flex; color: black; cursor: pointer;">
-            <!-- <input type="text" value="Dashboard"> -->
-            <button style="padding: 4px 15px; margin: 3px auto">
+        <div class="c-b dB-2">
+            <button class="btn">
                 Fermer</button>
         </div>
-        <div style="width: 98vw; height: 95vh; 
-        background-color: white;border-radius: 15px;
-        padding: 8px; color: #000;">
+        <div class="bg-w c-b dB-3">
         This is Dashboard
         </div>
     </div>
 </template>
+<style scoped>
+.bg-w{
+    background-color: white; 
+}
+.c-b{
+    color: black;
+}
+.dB{
+
+}
+.dB-1{
+    position:absolute;
+    left: 80%;top:4px;
+    justify-items: center;
+    height: 25px; padding: 3px 2rem;
+    border-top-left-radius: 5px; border-top-right-radius: 5px;
+}
+.dB-2{
+    display: flex; 
+}
+.dB-3{
+    width: 98vw; height: 95vh; 
+    border-radius: 15px;
+    padding: 8px; 
+}
+.btn{
+    padding: 4px 15px; 
+    margin: 3px auto;
+}
+.btn:active{
+    scale: 1.2;
+}
+</style>
