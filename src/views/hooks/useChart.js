a781678931615611f332cec76b34be38b1f68190
JovinMtl <nsanzumukizath@gmail.com>
2024-12-19 10:42:35 +0200

    made useChart composable

---
+++ src/views/hooks/useChart.js
@@ -0,0 +1,28 @@
+// This module is designed to consume data for Chart purpose
+
+import { ref } from "vue";
+import { baseURL } from "@/store/host";
+import { useUserStore } from "../../store/user";
+
+const data = ref(null) 
+
+export async function useChart(prefix='', command=null){
+    const { getAccessToken } = useUserStore();
+
+    const askData = async (prefix)=>{
+        let response = ''
+        try{
+            response = await fetch(`${baseURL}/${prefix}`, {
+                headers: {
+                    Authorization: "Bearer " + getAccessToken(),
+                },
+            });
+            if(response.ok){
+                data.value = await response.json()
+            }
+        } catch(error){
+            console.log("Failed because:", error)
+        }
+    }
+    return [data, askData]
+}
