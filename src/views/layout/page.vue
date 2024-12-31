4fb8f4f880033a3861890096c009220bb6be18f0
JovinMtl <nsanzumukizath@gmail.com>
2024-12-31 12:56:31 +0200

    removing readableNumber function

--- src/views/layout/page.vue
+++ src/views/layout/page.vue
@@ -770,25 +770,7 @@ const decrementQte = (value) => {
     }
 
 }
-const number_To_string = (value = 10000) => {
-    // This function is designed to format a number as a string
-    // in a format of separation by 3digits
-    let to_convert = value.toString()
-    let data_length = to_convert.length
-    let i = 0
-    let converted = ''
-    let reversed = ''
-    reversed = to_convert.split('').reverse().toString().replaceAll(',', '')
-    for (i = 0; i < data_length; i++) {
-        if (i % 3 == 0) {
-            converted = converted.concat(`.${reversed[i]}`)
-        } else {
-            converted = converted.concat(`${reversed[i]}`)
-        }
-    }
-    converted = converted.replace('.', '').split('').reverse().toString().replaceAll(',', '')
-    return converted
-}
+
 const readableNumber = (value:number=1000):String=>{
     let data = String(value)
     let r_data = data.split('').reverse()
