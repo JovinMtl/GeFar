f2b107eb80627858b8a460750810d47284fcd365
JovinMtl <nsanzumukizath@gmail.com>
2025-01-10 13:47:37 +0200

    first made clInfo

--- src/views/operations/reduction/cl-info.vue
+++ src/views/operations/reduction/cl-info.vue
@@ -1,6 +1,24 @@
 <template>
-    <div></div>
+    <div>
+        <input type="text" 
+            placeholder="Nom de l'adhérant" />
+        <input type="text"
+            placeholder="Employeur" />
+        <select v-model="relation">
+            <option>Lui-même</option>
+            <option>Conjoint</option>
+            <option>Enfant</option>
+        </select>
+        <br>
+        <label for="">Nom du benéficiaire</label>
+        <input type="text"
+            placeholder="beneficiaire" />
+        <input type="button" value="Ok"/>
+    </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
+
+const relation = ref("Lui-même")
 </script>
