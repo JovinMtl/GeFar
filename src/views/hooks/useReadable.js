9e5b08bf5aa9d845be63b2f48e4ec70e675a2175
JovinMtl <nsanzumukizath@gmail.com>
2025-01-18 07:09:16 +0200

    creating a composable named readableNumber

---
+++ src/views/hooks/useReadable.js
@@ -0,0 +1,22 @@
+
+
+export default function readableNumber (value=1000){
+    // makes 1000000 into a string of 1.000.000
+    let data = String(value)
+    let r_data = data.split('').reverse()
+    let len = data.length
+    let result = ''
+    for(let i=0; i <= len-1; i++){
+        if(i && i%3==0){
+            result = result.concat(`.${r_data[i]}`)
+        } else{
+            result = result.concat(r_data[i])
+        }
+    }
+    let len2 = result.length
+    let result2 = new String()
+    for (let i=len2-1; i>=0; i--){
+        result2 = result2.concat(result[i])
+    }
+    return result2
+}
