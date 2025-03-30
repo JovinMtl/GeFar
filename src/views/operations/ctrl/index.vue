9a8d5ca4d122b48be7792b7801c66453a71780b0
JovinMtl <nsanzumukizath@gmail.com>
2025-03-30 08:02:02 +0200

    factorized index by elt

--- src/views/operations/ctrl/index.vue
+++ src/views/operations/ctrl/index.vue
@@ -3,39 +3,39 @@
                 
                 <div class="controlHeader" style="height: 5%; width: 100%;">
                     <div  class="controlContent">
-                        <div class="contentElement1" style="display: inline-flex;background-color: transparent; width: 4%;height: 100%; color: green; ">
+                        <div class="elt contentElement1" style="display: inline-flex;background-color: transparent; width: 4%;height: 100%; color: green; ">
                             #
                         </div> 
-                        <div class="contentElement2">
+                        <div class="elt contentElement2">
                             Nom du Med.
                         </div> 
-                        <div class="contentElement3">
+                        <div class="elt contentElement3">
                             Quantite
                         </div>
 
-                        <div class="contentElement4">
+                        <div class="elt contentElement4">
                             <span v-if="isAdmin">Px.Achat</span>
                             <span v-else>T. Med.</span>
                             
                         </div>
 
-                        <div class="contentElement3">
+                        <div class="elt contentElement3">
                             Px. Vente
                         </div>
 
-                        <div class="contentElement3">
+                        <div class="elt contentElement3">
                             Total
                         </div>
 
-                        <div class="contentElement4">
+                        <div class="elt contentElement4">
                         <span v-if="isAdmin">Bénéfice</span>
                         <span v-else>Loc.</span>  
                         </div>
 
-                        <div class="contentElement4">
+                        <div class="elt contentElement4">
                             Date
                         </div>
-                        <div class="contentElement4">
+                        <div class="elt contentElement4">
                             Auteur
                         </div>
                     </div>
@@ -50,38 +50,38 @@
                         <div class="contentElement11">
                             {{ index + 1 }}
                         </div> 
-                        <div class="contentElement2">
+                        <div class="elt contentElement2">
                             {{ umuti.name_umuti }}
-                        </div> <div class="contentElement3">
+                        </div> <div class="elt contentElement3">
                             {{ (umuti.quantite_restant || umuti.quantity ) }}
                         </div> 
 
-                        <div class="contentElement4 famille_med">
+                        <div class="elt contentElement4 famille_med">
                             <span v-if="isAdmin">{{ umuti.price_in }}</span>
                             <span v-else>{{ umuti.famille_med }}</span>
                             
                         </div>
 
-                        <div class="contentElement3">
+                        <div class="elt contentElement3">
                             {{ umuti.price_out }}
                         </div>
 
-                        <div class="contentElement3 total">
+                        <div class="elt contentElement3 total">
                             {{ umuti.price_out * (umuti.quantite_restant || umuti.quantity) }}
                         </div>
 
-                        <div class="contentElement4"> 
+                        <div class="elt contentElement4"> 
                         <span v-if="isAdmin">{{ (umuti.price_out - umuti.price_in) * (umuti.quantite_restant || umuti.quantity) }}</span> 
                         <span v-else>{{ umuti.location }}</span>
                         </div>
 
-                        <div class="contentElement4">
+                        <div class="elt contentElement4">
                             {{ (umuti.date_last_vente 
                             || umuti.date_operation || 
                             umuti.date_winjiriyeko ||
                             '_____________').slice(6,10) }}
                         </div>
-                        <div class="contentElement4">
+                        <div class="elt contentElement4">
                             {{ umuti.operator }}
                         </div>
                     </div>
@@ -89,17 +89,17 @@
 
                 <div class="controlFooter">
                     <div  class="controlContent" style="font-weight: 700;">
-                        <div class="contentElement1">
+                        <div class="elt contentElement1">
                             #
                         </div> 
-                        <div class="contentElement2">
+                        <div class="elt contentElement2">
                             TOTAL
                         </div> 
-                        <div class="contentElement3">
+                        <div class="elt contentElement3">
                             {{ totaux[0] }}
                         </div>
 
-                        <div class="contentElement3">
+                        <div class="elt contentElement3">
                             <!-- {{ (totaux[1] / (totaux[0] || 1)).toFixed(1) }} -->
                               <!-- ---- -->
                               
@@ -107,20 +107,20 @@
                             <span v-else>----</span>
                         </div>
 
-                        <div class="contentElement3">
+                        <div class="elt contentElement3">
                             {{ totaux[1] }}
                         </div>
 
-                        <div class="contentElement4">
+                        <div class="elt contentElement4">
                             ----
                         </div>
 
-                        <div class="contentElement4">
+                        <div class="elt contentElement4">
                             <span v-if="isAdmin">{{ totaux[3] }}</span> 
                             <span v-else>----</span> 
                         </div>
 
-                        <div class="contentElement4">
+                        <div class="elt contentElement4">
                             ----
                         </div>
                     </div>
