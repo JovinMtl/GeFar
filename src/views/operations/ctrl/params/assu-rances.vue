fc12f484b2bb3a5e6a0d2386e00226a4d1515b99
JovinMtl <nsanzumukizath@gmail.com>
2025-04-03 12:56:15 +0200

    created assuRances and utiliSateur component

---
+++ src/views/operations/ctrl/params/assu-rances.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        Ici Assurances
+    </div>
+</template>
---
+++ src/views/operations/ctrl/params/utili-sateurs.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        Les Utilisateurs pourront etre gerees ici.
+    </div>
+</template>
