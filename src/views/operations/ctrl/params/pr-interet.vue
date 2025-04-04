f54f56d6fc52531216268328f5333f6b4a765d45
JovinMtl <nsanzumukizath@gmail.com>
2025-04-04 10:22:35 +0200

    styling the input elements of prInterest component

--- src/views/operations/ctrl/params/pr-interet.vue
+++ src/views/operations/ctrl/params/pr-interet.vue
@@ -1,6 +1,19 @@
 <template>
-    <h3>The title here</h3>
-    <div>
-        Ici Principe interet
+    <h3 style="color: white;">Vous pouvez mettre à jour votre principe d'Intéret.</h3>
+    <div class="co-inte">
+        <div>
+            <input style="text-align: center;" value="99" disabled><br>
+            <label>Actuel</label>
+        </div>
+        <div>
+            <input style="text-align: center;" 
+                placeholder="??" type="number"/><br>
+            <label>Nouveau</label>
+        </div>
+        <div>
+            <input type="button" value="Approuver"/>
+        </div>
+        
+
     </div>
 </template>
