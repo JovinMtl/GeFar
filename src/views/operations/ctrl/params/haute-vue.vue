0fa62746dcccbc3b18bde7ff172b627e325c6ba2
JovinMtl <nsanzumukizath@gmail.com>
2025-04-10 15:48:11 +0200

    created and described the use of hauteVue component

---
+++ src/views/operations/ctrl/params/haute-vue.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        Here we will enter an ID of a med and:
+        be able to see wether its presence in umutiEntree, UmutiSold, UmutiSet.
+        and be able to modify (prix_achat or prix_vente or date)
+    </div>
+</template>
