1ef3a228d1655e7c19bd5e220b7a5281ad845442
JovinMtl <nsanzumukizath@gmail.com>
2025-04-23 14:42:21 +0200

    finilized the form.

--- src/views/operations/ctrl/params/htv/updateAchat/m-achat.vue
+++ src/views/operations/ctrl/params/htv/updateAchat/m-achat.vue
@@ -5,12 +5,24 @@
         width: 100%; height: 60%; display: flex;
         justify-content: right;"> 
         <div class="bg-ht" style="max-width: 30%; height: 150%;overflow: scroll;">
-            here we modify achats: {{ props.umutiData }}
+            <!-- here we modify achats: {{ props.umutiData }} -->
+             <label for="">Date Per.</label>
+             <input type="date" v-model="date_peremption" /><br>
+             <label for="">Prix A.</label>
+             <input type="number" v-model="prix_achat"/><br>
+             <label for="">Qte</label>
+             <input type="number" v-model="quantite_initial"><br>
+             <input type="button" value="Confirmer" />
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
 
 const props = defineProps(['umutiData']);
+
+const date_peremption = ref(props.umutiData.date_peremption)
+const prix_achat = ref(props.umutiData.prix_achat)
+const quantite_initial = ref(props.umutiData.quantite_initial)
 </script>
