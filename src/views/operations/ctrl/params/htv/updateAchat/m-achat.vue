f7da903a13a7709fee1408c9f3fb3b8079d152be
JovinMtl <nsanzumukizath@gmail.com>
2025-04-23 18:25:08 +0200

    able to update achat.

--- src/views/operations/ctrl/params/htv/updateAchat/m-achat.vue
+++ src/views/operations/ctrl/params/htv/updateAchat/m-achat.vue
@@ -4,25 +4,66 @@
         margin-top: 15px;
         width: 100%; height: 60%; display: flex;
         justify-content: right;"> 
-        <div class="bg-ht" style="max-width: 30%; height: 150%;overflow: scroll;">
+        <div class="bg-ht" style="max-width: 30%; max-height: 150%;overflow: scroll;">
             <!-- here we modify achats: {{ props.umutiData }} -->
+             <!-- <br> -->
              <label for="">Date Per.</label>
              <input type="date" v-model="date_peremption" /><br>
              <label for="">Prix A.</label>
              <input type="number" v-model="prix_achat"/><br>
              <label for="">Qte</label>
              <input type="number" v-model="quantite_initial"><br>
-             <input type="button" value="Confirmer" />
+             <input type="button" value="Confirmer" @click="updateAchat"/><br>
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { reactive, ref, toValue, watch } from 'vue'
+import { useKurungika } from '../../../../../hooks/kuvoma'
 
 const props = defineProps(['umutiData']);
 
 const date_peremption = ref(props.umutiData.date_peremption)
 const prix_achat = ref(props.umutiData.prix_achat)
 const quantite_initial = ref(props.umutiData.quantite_initial)
+const code_med = props.umutiData.code_med;
+const code_operation = props.umutiData.code_operation;
+
+const objAPI = reactive({
+    'request': 'post',
+    'code_med': props.umutiData.code_med,
+    'code_operation':  props.umutiData.code_operation,
+    'quantite_initial': 0,
+    'prix_achat': 0,
+    'date_peremption': 0
+})
+const updateAchat = ()=>{
+    objAPI.request = 'post';
+    objAPI.quantite_initial = toValue(quantite_initial);
+    objAPI.prix_achat = toValue(prix_achat);
+    objAPI.date_peremption = toValue(date_peremption);
+
+    updateAchatEntree()
+}
+const oneCompiled_url = 'api/gOps/setPachatEntree/'
+const [oneCompiled, updateAchatEntree] = useKurungika(objAPI, oneCompiled_url)
+
+
+watch(oneCompiled, (value)=>{
+    if (value.response == 200){
+        console.log("It is well done")
+    } else if(value.response == 404){
+        console.log("Not found")
+    }
+})
 </script>
+
+<style scoped>
+input{
+    background-color: black;
+}
+label{
+    color: blue;
+}
+</style>
