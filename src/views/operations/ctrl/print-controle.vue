4344e2029b89fbcb3828fdc58fc5fdb8ef58cf41
JovinMtl <nsanzumukizath@gmail.com>
2025-05-03 10:00:38 +0200

    initialized the printControle template

---
+++ src/views/operations/ctrl/print-controle.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="prMainContainer">
+        <!-- Manage to receive the inputs likely imiti_S,
+         and adapt for printing -->
+    </div>
+</template>
+<script setup lang="ts">
+</script>
+<style scoped>
+.prMainContainer{
+    width: 100%;
+    height: 100%;
+    background-color: white;
+}
+</style>
