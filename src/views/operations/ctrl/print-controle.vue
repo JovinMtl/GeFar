5f2508bf46a4aedc8c5b2e99c1e7a32eb4f212b2
JovinMtl <nsanzumukizath@gmail.com>
2025-05-03 11:49:43 +0200

    dynamically can the title

--- src/views/operations/ctrl/print-controle.vue
+++ src/views/operations/ctrl/print-controle.vue
@@ -2,15 +2,29 @@
     <div class="prMainContainer">
         <!-- Manage to receive the inputs likely imiti_S,
          and adapt for printing -->
+        <div>
+            <h3>Here for printing</h3>
+        </div>
+        <div>
+            <slot></slot>
+        </div>
+         
     </div>
 </template>
 <script setup lang="ts">
+import { useTitle } from '@vueuse/core'
+
+const props = defineProps(['pageTitle',])
+
+const title = useTitle()
+title.value = props['pageTitle']
 </script>
 <style scoped>
 .prMainContainer{
     width: 100%;
     height: 100%;
-    background-color: white;
+    background-color: rgba(255, 255, 255, 1);
+    color: black;
     position: absolute;
 }
 </style>
