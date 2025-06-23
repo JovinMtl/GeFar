84ab15c335be5d41dab8b67521fcd437a9767fc7
JovinMtl <nsanzumukizath@gmail.com>
2025-06-23 22:33:18 +0200

    autofill the infos gotten from the local server.

--- src/views/operations/ctrl/params/in-fo.vue
+++ src/views/operations/ctrl/params/in-fo.vue
@@ -9,7 +9,9 @@
                         <label for="">
                             Nom
                         </label>
-                        <input type="text" placeholder="Nom de la Pharmacie"/>
+                        <input type="text" 
+                            placeholder="Nom de la Pharmacie"
+                            v-model="data.name_pharma"/>
                     </td>
                 </tr>
             </thead>
@@ -19,7 +21,7 @@
                         <label for="">
                             Tél
                         </label>
-                        <input type="text" placeholder="Contact"/>
+                        <input type="text" placeholder="Contact" v-model="data.tel"/>
                     </td>
                 </tr>
                 <tr>
@@ -27,7 +29,9 @@
                         <label for="">
                             Avenue
                         </label>
-                        <input type="text" placeholder="description courte (5 lettres)"/>
+                        <input type="text" 
+                            placeholder="description courte (5 lettres)"
+                            v-model="data.loc_street"/>
                     </td>
                 </tr>
                 <tr>
@@ -35,7 +39,8 @@
                         <label for="">
                             Quartier
                         </label>
-                        <input type="text" placeholder="_"/>
+                        <input type="text" placeholder="_"
+                            v-model="data.loc_quarter"/>
                     </td>
                 </tr>
                 <tr>
@@ -43,7 +48,8 @@
                         <label for="">
                             Commune
                         </label>
-                        <input type="text" placeholder="_"/>
+                        <input type="text" placeholder="_"
+                            v-model="data.loc_commune"/>
                     </td>
                 </tr>
                 <tr>
@@ -51,7 +57,8 @@
                         <label for="">
                             Province
                         </label>
-                        <input type="text" placeholder="_"/>
+                        <input type="text" placeholder="_"
+                            v-model="data.loc_Province"/>
                     </td>
                 </tr>
             </tbody>
@@ -61,7 +68,8 @@
                         <label for="">
                             Mot de passe
                         </label>
-                        <input type="password" placeholder="_"/>
+                        <input type="password" placeholder="_"
+                            v-model="data.remote_password"/>
                     </td>
                 </tr>
                 <tr>
@@ -69,7 +77,8 @@
                         <label for="">
                             Mot de passe
                         </label>
-                        <input type="password" placeholder="_"/>
+                        <input type="password" placeholder="_"
+                            v-model="data.remote_password2"/>
                     </td>
                 </tr>
             </tfoot>
@@ -80,6 +89,30 @@
         <button class="clk">Confirmer</button>
     </div>
 </template>
+<script setup lang="ts">
+import { reactive, ref, watch } from 'vue';
+import { useKuvoma } from '../../../hooks/kuvoma';
+import { useKurungikaRemote } from '../../../hooks/kuvoma';
+
+const data = reactive({})
+
+const url_request_info = "api/gOps2/request_infos/"
+const [infos, requestInfo] = useKuvoma(url_request_info)
+requestInfo()
+
+//watchers
+watch(infos, (value)=>{
+    data.name_pharma = value.response.name_pharma
+    data.tel = value.response.tel
+    data.loc_street = value.response.loc_street
+    data.loc_quarter = value.response.loc_quarter
+    data.loc_commune = value.response.loc_commune
+    data.loc_Province = value.response.loc_Province
+    data.remote_password = value.response.remote_password
+    data.remote_password2 = value.response.remote_password
+    console.log("infos got: " + JSON.stringify(data.name_pharma))
+})
+</script>
 
 <style scoped>
 .info-class{
