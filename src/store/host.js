a7c0aceca5f7b893c2b904ab5ee23c20a5c5e3e5
JovinMtl <nsanzumukizath@gmail.com>
2025-07-25 16:16:03 +0200

    made the remoteURL to be of pythonAnyWhere

--- src/store/host.js
+++ src/store/host.js
@@ -1,6 +1,6 @@
 
 export const baseURL = "//127.0.0.1:8002"
-export const remoteURL = "//127.0.0.1:8008"
+// export const remoteURL = "//127.0.0.1:8008"
 // export const remoteURL = "https://5hmc28-8008.csb.app"
 
-// export const baseURL = "https://muteule.pythonanywhere.com"
+export const remoteURL = "https://muteule.pythonanywhere.com"
