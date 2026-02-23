440926c338a635ca778cd439f83dddc582c9e025
JovinMtl <nsanzumukizath@gmail.com>
2026-02-23 13:39:17 +0200

    created a component that will update qte of umutiEntree

---
+++ src/views/operations/ctrl/params/htv/updateAchat/q-te/q-te.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        
+        <label> Ancien Qté : 
+            <span class="c-b">{{ (props.umutiData?.prix_achat) }}</span> 
+        </label>
+        <hr>
+        <input type="number" placeholder="Nouveau Px.Achat"/>
+        <div>
+            <button class="btn bg-b-1">Ok</button>
+        </div>
+    </div>
+</template>
+<script setup lang="ts">
+    import { ref, watch } from 'vue'
+    const props = defineProps(['umutiData']);
+</script>
