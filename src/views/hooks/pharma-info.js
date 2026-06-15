e30b942fe7fb84e8de923ae677419b5329652316
JovinMtl <nsanzumukizath@gmail.com>
2026-06-15 11:03:25 +0200

    able to export phNif

--- src/views/hooks/pharma-info.js
+++ src/views/hooks/pharma-info.js
@@ -6,19 +6,27 @@
 // June 15, 2026. Thierry Nsanzumukiza
 //
 
+// The States
 const ph_name = {
   ubz: "Pharmacie Ubuzima",
   shn: "Shana Pharma",
 };
 
 const ph_address = {
-  ubz: "13av. Twinyoni, Kamenge, Ntahangwa",
-  shn: "7av, Gikizi, Kamenge, Ntahangwa",
+  ubz: "13è Av, Q. Twinyoni, Kamenge. Ntahangwa - Bujumbura",
+  shn: "7è Av, Q. Gikizi, Kamenge, Ntahangwa - Bujumbura",
 };
 
+const ph_nif = {
+  ubz: "4001411976",
+  shn: "0000000000",
+};
+
+// The target to be used.
 const selectedPharma = "shn";
 const emptyStr = " ";
 
 // States to export
 export const phName = ph_name[selectedPharma] ?? emptyStr;
 export const phAddress = ph_address[selectedPharma] ?? emptyStr;
+export const phNif = ph_nif[selectedPharma] ?? emptyStr;
